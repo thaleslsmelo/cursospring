5082c9b1ff9fbb304a63c7c69ca2014b64bfa04b
thales.melo <thales.melo@mv.com.br>
2025-06-12 13:17:45 -0300

    Projeto criado

---
+++ src/test/java/com/aulaspringweb/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringweb.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
