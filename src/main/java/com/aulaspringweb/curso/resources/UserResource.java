be0d60da43f7aac9e041b5803b278bfa63e02a3a
thales.melo <thales.melo@mv.com.br>
2025-06-12 13:59:05 -0300

    primeira aula

---
+++ src/main/java/com/aulaspringweb/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.aulaspringweb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulaspringweb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Thales","thales.com.br","989898","123456");
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
