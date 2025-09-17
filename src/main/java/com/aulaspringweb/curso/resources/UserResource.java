f192cfb4f16610a1e4b4d3f6c5a1b798fa891635
Thales <thaleslucasdemelo@gmail.com>
2025-09-17 07:04:17 -0300

    Created method User delete

--- src/main/java/com/aulaspringweb/curso/resources/UserResource.java
+++ src/main/java/com/aulaspringweb/curso/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,7 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll(){
 		List<User> list = service.findAll();
  		//User u = new User(1L,"Thales","thales.com.br","989898","123456");
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(list); 
 		
 	}
 	
@@ -43,8 +44,13 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
-		
-		
+			
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return  ResponseEntity.noContent().build();
 	}
 
 }
